58a336d555f219ba5b2e9e8366c5b304f2a6db1d
Arthur Hoek <arthur@arthurhoek.nl>
2017-02-05 13:14:20 +0100

    Add JLPT level form control

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,12 @@ import {Component} from '@angular/core';
 
 import {NavController} from 'ionic-angular';
 
+import {
+  FormGroup,
+  FormControl
+
+} from '@angular/forms';
+
 import {ReviewPage} from '../review/review';
 
 @Component({
@@ -10,14 +16,16 @@ import {ReviewPage} from '../review/review';
 })
 
 export class HomePage {
+    
+    settingsForm: any;
 
     constructor(public navCtrl: NavController) {
-
+        this.settingsForm = new FormGroup({
+            "jlptLevel": new FormControl({value: 'N5', disabled: false})
+        });
     }
 
     startReview() {
         this.navCtrl.push(ReviewPage, {});
     }
-
-
 }
