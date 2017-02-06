876592438fe17f3c9c9562605fa6b05159aa5aac
Arthur Hoek <arthur@arthurhoek.nl>
2017-02-06 13:06:22 +0100

    Select 10 random words for the questions

--- src/providers/question-data.ts
+++ src/providers/question-data.ts
@@ -29,30 +29,32 @@ export class QuestionData {
 
         return new Promise(resolve => {
             this.http.get('assets/data/questions/words.json').map(res => res.json()).subscribe(data => {
-                var words: Array<any> = data.data;
-                var questions: Array<any> = [];
+                var allWords: Array<any> = data.data;
 
-                var question;
-                // Convert the data to settings
-                for (var i = 0; i < words.length; i++) {
-                    //console.log(words[i])
+                var questions: Array<any> = [];
+                var word: any;
+
+                for (var i = 0; i < 10; i++) {
+                    word = this.getRandomItem(allWords);
+                    var question;
+                    console.log(word)
 
                     question = {
-                        dictionary: words[i].japanese[0].word,
-                        furigana: words[i].japanese[0].reading,
-                        meaning: words[i].senses[0].english_definitions[0],
+                        dictionary: word.japanese[0].word,
+                        furigana: word.japanese[0].reading,
+                        meaning: word.senses[0].english_definitions[0],
                         answer: ''
                     };
+
                     // Suru verb
-                    // 
-                    if (words[i].senses[0].parts_of_speech[0] == 'Noun') {
-                    
-                        question.dictionary = words[i].japanese[0].reading + 'する';
+                    if (word.senses[0].parts_of_speech[0] == 'Noun') {
+
+                        question.dictionary = word.japanese[0].reading + 'する';
                         question.furigana = null;
-                        
+
                     }
 
-                    question.answer = this.getTeForm(words[i]);
+                    question.answer = this.getTeForm(word);
 
                     questions.push(question);
                 }
@@ -113,5 +115,14 @@ export class QuestionData {
         teForm = verbWithoutEnd + teEnding;
         console.log(reading, teForm)
         return teForm;
+    }
+
+    /**
+     * Get a random item from an array and remove it from the array
+     */
+    getRandomItem(items: Array<any>) {
+        var randomIndex = Math.floor(Math.random() * items.length);
+        var item = items.splice(randomIndex, 1);
+        return item[0];
     }
 }
