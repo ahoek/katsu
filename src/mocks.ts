9e75c310893fa19fdceeff2242435ac32db5b1a2
Arthur Hoek <arthur@arthurhoek.nl>
2017-02-15 23:30:07 +0100

    Add test configuration

---
+++ src/mocks.ts
@@ -0,0 +1,92 @@
+/* tslint:disable */ 
+// IONIC:
+
+export class ConfigMock {
+
+  public get(): any {
+    return '';
+  }
+
+  public getBoolean(): boolean {
+    return true;
+  }
+
+  public getNumber(): number {
+    return 1;
+  }
+}
+
+export class FormMock {
+  public register(): any {
+    return true;
+  }
+}
+
+export class NavMock {
+
+  public pop(): any {
+    return new Promise(function(resolve: Function): void {
+      resolve();
+    });
+  }
+
+  public push(): any {
+    return new Promise(function(resolve: Function): void {
+      resolve();
+    });
+  }
+
+  public getActive(): any {
+    return {
+      'instance': {
+        'model': 'something',
+      },
+    };
+  }
+
+  public setRoot(): any {
+    return true;
+  }
+}
+
+export class PlatformMock {
+  public ready(): Promise<{String}> {
+    return new Promise((resolve) => {
+      resolve('READY');
+    });
+  }
+
+  public registerBackButtonAction(fn: Function, priority?: number): Function {
+    return (() => true);
+  }
+
+  public hasFocus(ele: HTMLElement): boolean {
+    return true;
+  }
+
+  public doc(): HTMLDocument {
+    return document;
+  }
+
+  public registerListener(ele: any, eventName: string, callback: any): Function {
+    return (() => true);
+  }
+
+  public win(): Window {
+    return window;
+  }
+
+  public raf(callback: any): number {
+    return 1;
+  }
+}
+
+export class MenuMock {
+  public close(): any {
+    return new Promise((resolve: Function) => {
+      resolve();
+    });
+  }
+}
+
+/* tslint:enable */
