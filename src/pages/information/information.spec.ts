11dd624396ffcd5a3eb216e8bfc05fa9606f3334
Arthur Hoek <arthur@arthurhoek.nl>
2017-04-02 22:15:58 +0200

    Add simple component test

---
+++ src/pages/information/information.spec.ts
@@ -0,0 +1,38 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+import {DebugElement} from '@angular/core';
+import {InformationPage} from './information';
+import {IonicModule, NavController} from 'ionic-angular/index';
+
+describe('InformationPage', function () {
+    let de: DebugElement;
+    let comp: InformationPage;
+    let fixture: ComponentFixture<InformationPage>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [InformationPage],
+            imports: [
+                IonicModule.forRoot(InformationPage)
+            ],
+            providers: [
+                NavController
+            ]
+        });
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(InformationPage);
+        comp = fixture.componentInstance;
+        de = fixture.debugElement.query(By.css('h2'));
+    });
+
+    it('should create component', () => expect(comp).toBeDefined());
+
+    it('should have expected <h2> text', () => {
+        fixture.detectChanges();
+        const h2 = de.nativeElement;
+        expect(h2.innerText).toMatch(/how/i,
+            '<h2> should say something about "How"');
+    });
+});
--- src/pages/information/information.ts
+++ src/pages/information/information.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 
 /*
   Generated class for the Information page.
@@ -13,7 +13,7 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class InformationPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad InformationPage');
