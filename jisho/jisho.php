a92af47fce6f7344b188a335bc69fa5fb438f6cb
Arthur Hoek <arthur@arthurhoek.nl>
2017-07-12 22:02:05 +0200

    Get new definitions from Jisho

--- jisho/jisho.php
+++ jisho/jisho.php
@@ -4,16 +4,14 @@
  * Get the words via the Jisho api
  */
 $j = new JishoScraper();
-//$words = $j->getWordsOfLevel('n5');
-//$words = $j->getWordsOfLevel('n4');
-//$words = $j->getWordsOfLevel('n3');
-//$words = $j->getWordsOfLevel('n2');
-$words = $j->getWordsOfLevel('n1');
-var_dump("#".count($words));
+$j->getWordsOfLevel('n5');
+$j->getWordsOfLevel('n4');
+$j->getWordsOfLevel('n3');
+$j->getWordsOfLevel('n2');
+$j->getWordsOfLevel('n1');
 
 class JishoScraper
 {
-
     protected $baseUrl = 'http://jisho.org/api/v1/search/words';
     protected $words = [];
     protected $baseRequest = '';
@@ -37,17 +35,22 @@ class JishoScraper
         foreach ($types as $type) {
             $this->baseRequest = $this->baseUrl.'?keyword='.urlencode('#jlpt-'.$level.' #'.$type);
             var_dump($this->baseRequest);
-            $words = $this->recursiveRequest(1);
+            $words = $this->getWordsFromPage(1);
             $fileObject->$type = $words;
         }
 
         $json = json_encode($fileObject, JSON_UNESCAPED_UNICODE);
         file_put_contents('../src/assets/data/questions/words-'.$level.'.json', $json);
-
-        return $words;
+        var_dump("#".count($words));
     }
 
-    public function recursiveRequest($page = 1)
+    /**
+     * Get the words from page $page and above
+     * 
+     * @param int $page
+     * @return array
+     */
+    protected function getWordsFromPage($page = 1)
     {
         var_dump("page $page");
         $response = file_get_contents($this->baseRequest.'&page='.$page);
@@ -57,7 +60,7 @@ class JishoScraper
         if (count($words) > 0 && $page < 100) {
             // Don't spam the server
             sleep(3);
-            $nextWords = $this->recursiveRequest($page + 1);
+            $nextWords = $this->getWordsFromPage($page + 1);
             if (is_array($nextWords)) {
                 $words = array_merge($words, $nextWords);
             }
