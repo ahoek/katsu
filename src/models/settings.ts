cced3a6fca15088a3bdfffe21d99e7ae998bc0c2
Arthur Hoek <arthur@arthurhoek.nl>
2018-01-26 23:22:24 +0100

    Make settings more robust

--- src/models/settings.ts
+++ src/models/settings.ts
@@ -3,16 +3,9 @@
  */
 export class Settings {
 
-    private _verb: boolean;
-    get verb(): boolean {
-        return this._verb;
-    }
-    set verb(value) {
-        this._verb = value;
-    }
-    
-    iAdjective: boolean;
-    naAdjective: boolean;
+    verb: true;
+    iAdjective: false;
+    naAdjective: false;
 
     private _normal: boolean;
     get normal(): boolean {
@@ -21,30 +14,104 @@ export class Settings {
     set normal(value) {
         this._normal = value;
         if (this._normal) {
-            if (!this.polite && !this.plain) {
-                this.polite = true;
-            }
-            if (!this.past && !this.nonPast) {
-                this.nonPast = true;
-            }
-            if (!this.positive && !this.negative) {
-                this.positive = true;
-            }
+            this.needsPartOfSpeech();
+            this.needsSpeechLevel();
+            this.needsTense();
+            this.needsModality();
         }
     }
         
-    teForm: boolean;
-    volitional: boolean;
-    taiForm: boolean;
-    potential: boolean;
-    imperative: boolean;
-    conditional: boolean;
-    tariForm: boolean;
+    private _teForm: boolean;
+    get teForm(): boolean {
+        return this._teForm;
+    }
+    set teForm(value) {
+        this._teForm = value;
+        if (this._teForm) {
+            this.needsPartOfSpeech();
+        }
+    }
+    
+    private _volitional: boolean;
+    get volitional(): boolean {
+        return this._volitional;
+    }
+    set volitional(value) {
+        this._volitional = value;
+        if (this._volitional) {
+            this.needsVerb();
+            this.needsSpeechLevel();
+        }
+    }
 
+    private _taiForm: boolean;
+    get taiForm(): boolean {
+        return this._taiForm;
+    }
+    set taiForm(value) {
+        this._taiForm = value;
+        if (this._taiForm) {
+            this.needsVerb();
+            this.needsSpeechLevel();
+            this.needsTense();
+            this.needsModality();
+        }
+    } 
+       
+    private _potential: boolean;
+    get potential(): boolean {
+        return this._potential;
+    }
+    set potential(value) {
+        this._potential = value;
+        if (this._potential) {
+            this.needsVerb();
+            this.needsSpeechLevel();
+        }
+    }
+        
+    private _imperative: boolean;
+    get imperative(): boolean {
+        return this._imperative;
+    }
+    set imperative(value) {
+        this._imperative = value;
+        if (this._imperative) {
+            this.needsVerb();
+            this.needsModality();
+        }
+    }    
+
+    private _conditional: boolean;
+    get conditional(): boolean {
+        return this._conditional;
+    }
+    set conditional(value) {
+        this._conditional = value;
+        if (this._conditional) {
+            this.needsPartOfSpeech();
+            this.needsModality();
+        }
+    }
+    
+    private _tariForm: boolean;
+    get tariForm(): boolean {
+        return this._tariForm;
+    }
+    set tariForm(value) {
+        this._tariForm = value;
+        if (this._tariForm) {
+            this.needsVerb();
+            this.needsModality();
+        }
+    }
+    
     polite: boolean;
     plain: boolean;
+    
     past: boolean;
     nonPast: boolean;
+    
     positive: boolean;
     negative: boolean;
 
@@ -54,16 +121,43 @@ export class Settings {
 
     constructor() {
     }
+    
+    private needsVerb() {
+        if (!this.verb) {
+            this.verb = true;
+        }
+    }
+    
+    private needsPartOfSpeech() {
+        if (!this.verb && !this.iAdjective && !this.naAdjective) {
+            this.verb = true;
+        }
+    }
+    
+    private needsSpeechLevel() {
+        if (!this.polite && !this.plain) {
+            this.polite = true;
+        }
+    }
+    
+    private needsTense() {
+        if (!this.past && !this.nonPast) {
+            this.nonPast = true;
+        }
+    }
+    
+    private needsModality() {
+        if (!this.positive && !this.negative) {
+            this.positive = true;
+        }
+    }
 
     /**
      * Get the default settings
      */
     static getDefault(): Settings {
         const settings = new Settings();
-        settings.verb = true;
-        settings.iAdjective = false;
-        settings.naAdjective = false;
-        
+
         settings.normal = true;
         settings.teForm = false;
         settings.volitional = false;
@@ -73,10 +167,16 @@ export class Settings {
         settings.conditional = false;
         settings.tariForm = false;
         
+        settings.verb = true;
+        settings.iAdjective = false;
+        settings.naAdjective = false;
+        
         settings.polite = true;
         settings.plain = true;
+        
         settings.past = true;
         settings.nonPast = true;
+        
         settings.positive = true;
         settings.negative = true;
 
@@ -87,6 +187,157 @@ export class Settings {
         return settings;
     }
     
-    static check(settings) {
+    /** 
+     * Get the available question options
+     */
+    getQuestionTypeOptions(): string[] {
+        let options: string[] = [];
+
+        this.addNormal(options);
+        this.addTeForm(options);     
+        
+        if (this.volitional) {
+            if (this.plain) {
+                options.push('volitional-plain');
+            }
+            if (this.polite) {
+                options.push('volitional-polite');
+            }
+        }
+        
+        if (this.potential) {
+            if (this.plain) {
+                options.push('potential-plain-positive-present');
+            }
+            if (this.polite) {
+                options.push('potential-polite-positive-present');
+            }
+        }
+        
+        if (this.imperative) {
+            if (this.positive) {
+                options.push('imperative-positive');
+            }
+            if (this.negative) {
+                options.push('imperative-negative');
+            }
+        }
+        
+        this.addConditional(options);
+        
+        if (this.taiForm) {
+            this.addSubOptionsFor('tai-form', options);
+        }
+        
+        if (this.tariForm) {
+            if (this.positive) {
+                options.push('tari-form-positive');
+            }
+            if (this.negative) {
+                options.push('tari-form-negative');
+            }
+        }
+        
+        return options;
+    }
+    
+    addNormal(options: string[]) {
+        const form = 'normal';
+        if (!this.normal) {
+            return;
+        }
+        
+        if (this.verb) {
+            this.addOptionsFor(form + '-verb-', options);
+        }
+        if (this.iAdjective) {
+            this.addOptionsFor(form + '-i-adjective', options);
+        }
+        if (this.naAdjective) {
+            this.addOptionsFor(form + '-na-adjective-', options);
+        }
+    }
+
+    addTeForm(options: string[]) {    
+        const form = 'te-form';
+        if (!this.teForm) {
+            return;
+        }
+
+        if (this.verb) {
+            options.push(form + 'verb'); 
+        }
+        if (this.iAdjective) {
+            options.push(form + 'i-adjective'); 
+        }
+        if (this.naAdjective) {
+            options.push(form + 'na-adjective'); 
+        }
+    }
+    
+    addConditional(options: string[]) {
+        const form = 'conditional';
+        if (!this.conditional) {
+            return;
+        }
+        
+        if (this.positive) {
+            if (this.verb) {
+                options.push(form + '-verb-positive');
+            }
+            if (this.iAdjective) {
+                options.push(form + '-i-adjective-positive');
+            }
+            if (this.naAdjective) {
+                options.push(form + '-na-adjective-positive');
+            }
+        }
+        if (this.negative) {
+            if (this.verb) {
+                options.push(form + '-verb-negative');
+            }
+            if (this.iAdjective) {
+                options.push(form + '-i-adjective-negative');
+            }
+            if (this.naAdjective) {
+                options.push(form + '-na-adjective-negative');
+            }
+        } 
+    }
+    
+    /**
+     * Add options for plain and polite
+     */
+    addOptionsFor(base: string, options: string[]) {
+        if (this.normal) {
+            if (this.plain) {
+                this.addSubOptionsFor(base + 'plain', options);
+            }
+            if (this.polite) {
+                this.addSubOptionsFor(base + 'polite', options);
+            }
+        }
+    }
+    
+    /**
+     * Add past/nonpast and positive/negative options
+     */
+    addSubOptionsFor(base: string, options: string[]) {
+        if (this.positive) {
+            if (this.nonPast) {
+                options.push(base + '-positive-present');
+            }
+            if (this.past) {
+                options.push(base + '-positive-past');
+            }
+        }
+        if (this.negative) {
+            if (this.nonPast) {
+                options.push(base + '-negative-present');
+            }
+            if (this.past) {
+                options.push(base + '-negative-past');
+            }
+        }
     }
 }
